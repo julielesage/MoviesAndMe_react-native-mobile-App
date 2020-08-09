c53d15e221f587c15ba4a411c6a10434e92cff70
julielesage <lesage.julie@gmail.com>
2020-08-09 18:43:15 +0200

    feat : added redux-persist

--- App.js
+++ App.js
@@ -19,89 +19,94 @@ import Favorites from "./containers/Favorites";
 // npm install
 import { Provider } from "react-redux";
 import store from "./redux/store/configureStore";
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default class App extends React.Component {
   render() {
+    let persistor = persistStore(store);
     return (
       <Provider store={store}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            {/* Les screens retournent toujours des fonctions qui retourne un react-element ! */}
-            <Stack.Screen
-              name="Search"
-              options={{
-                title: "",
-                animationEnabled: true,
-                headerTintColor: "black",
-              }}
-            >
-              {() => (
-                <Tab.Navigator
-                  tabBarOptions={{
-                    activeTintColor: "blue",
-                    showLabel: false,
-                    activeBackgroundColor: "#DDDDDD",
-                    inactiveBackgroundColor: "#FFF",
-                  }}
-                >
-                  <Tab.Screen
-                    name="Search"
-                    options={{
-                      tabBarIcon: ({ size, color }) => (
-                        <FontAwesome name="search" size={24} color="black" />
-                      ),
+        <PersistGate persistor={persistor}>
+          <NavigationContainer>
+            <Stack.Navigator>
+              {/* Les screens retournent toujours des fonctions qui retourne un react-element ! */}
+              <Stack.Screen
+                name="Search"
+                options={{
+                  title: "",
+                  animationEnabled: true,
+                  headerTintColor: "black",
+                }}
+              >
+                {() => (
+                  <Tab.Navigator
+                    tabBarOptions={{
+                      activeTintColor: "blue",
+                      showLabel: false,
+                      activeBackgroundColor: "#DDDDDD",
+                      inactiveBackgroundColor: "#FFF",
                     }}
                   >
-                    {() => (
-                      <Stack.Navigator>
-                        <Stack.Screen
-                          name="Search"
-                          options={{
-                            title: "s",
-                            animationEnabled: true,
-                          }}
-                        >
-                          {() => <Search />}
-                        </Stack.Screen>
-                        <Stack.Screen
-                          name="Film Details"
-                          options={{
-                            title: "",
-                            animationEnabled: true,
-                            headerTintColor: "black",
-                            headerBackTitleVisible: true,
-                          }}
-                        >
-                          {(props) => <FilmDetails {...props} />}
-                        </Stack.Screen>
-                      </Stack.Navigator>
-                    )}
-                  </Tab.Screen>
+                    <Tab.Screen
+                      name="Search"
+                      options={{
+                        tabBarIcon: ({ size, color }) => (
+                          <FontAwesome name="search" size={24} color="black" />
+                        ),
+                      }}
+                    >
+                      {() => (
+                        <Stack.Navigator>
+                          <Stack.Screen
+                            name="Search"
+                            options={{
+                              title: "s",
+                              animationEnabled: true,
+                            }}
+                          >
+                            {() => <Search />}
+                          </Stack.Screen>
+                          <Stack.Screen
+                            name="Film Details"
+                            options={{
+                              title: "",
+                              animationEnabled: true,
+                              headerTintColor: "black",
+                              headerBackTitleVisible: true,
+                            }}
+                          >
+                            {(props) => <FilmDetails {...props} />}
+                          </Stack.Screen>
+                        </Stack.Navigator>
+                      )}
+                    </Tab.Screen>
 
-                  <Tab.Screen
-                    name="Favorites"
-                    options={{
-                      tabBarIcon: ({ size, color }) => (
-                        <FontAwesome name="heart" size={24} color="black" />
-                      ),
-                    }}
-                  >
-                    {() => (
-                      <Stack.Navigator>
-                        <Stack.Screen name="Favorites">
-                          {() => <Favorites />}
-                        </Stack.Screen>
-                      </Stack.Navigator>
-                    )}
-                  </Tab.Screen>
-                </Tab.Navigator>
-              )}
-            </Stack.Screen>
-          </Stack.Navigator>
-        </NavigationContainer>
+                    <Tab.Screen
+                      name="Favorites"
+                      options={{
+                        tabBarIcon: ({ size, color }) => (
+                          <FontAwesome name="heart" size={24} color="black" />
+                        ),
+                      }}
+                    >
+                      {() => (
+                        <Stack.Navigator>
+                          <Stack.Screen name="Favorites">
+                            {() => <Favorites />}
+                          </Stack.Screen>
+                        </Stack.Navigator>
+                      )}
+                    </Tab.Screen>
+                  </Tab.Navigator>
+                )}
+              </Stack.Screen>
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PersistGate>
       </Provider>
     );
   }
--- redux/store/configureStore.js
+++ redux/store/configureStore.js
@@ -1,8 +1,14 @@
 // création d'un store :
 
-import { createStore, combineReducers } from "redux";
+import { createStore } from "redux";
 import toggleFavorite from "../reducers/favoriteReducer";
 import setAvatar from "../reducers/setAvatar";
+import { persistCombineReducers } from 'redux-persist';
+import { AsyncStorage } from "react-native";
 
+const rootPersistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+}
 // let store = createStore(reducer);
-export default createStore(combineReducers({ toggleFavorite, setAvatar }));
+export default createStore(persistCombineReducers(rootPersistConfig, { toggleFavorite, setAvatar }));
